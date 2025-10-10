b50443187a1f2835a139a68abac7625cf21fb24f
GustavoSanchez <27065908+gushiro@users.noreply.github.com>
2025-10-10 17:59:41 +0900

    Delete calibration directory

--- calibration/decapodiforms_lsd2_runs.sh
+++
@@ -1,7 +0,0 @@
-~/programs/iqtree2/lsd2/bin/lsd2_unix -i bpcomp_cal1.tree -s 152405 -a 'b(-240,-245)' -f 100  -z 0 -u 1 -o lsd2_decapodiformes_rootedu_final_240_249_f100_tree1
-
-~/programs/iqtree2/lsd2/bin/lsd2_unix -i bpcomp_cal1.tree -d calibration_tree2.txt -s 152405 -a 'b(-240,-245)' -f 100  -z 0 -u 1 -o lsd2_decapodiformes_rootedu_final_240_249_f100_tree2
-
-~/programs/iqtree2/lsd2/bin/lsd2_unix -i bpcomp_cal1.tree -d calibration_tree3.txt -s 152405 -a 'b(-240,-245)' -f 100  -z 0 -u 1 -o lsd2_decapodiformes_rootedu_final_240_249_f100_tree3
-
-~/programs/iqtree2/lsd2/bin/lsd2_unix -i bpcomp_cal1.tree -d calibration_tree4.txt -s 152405 -a 'b(-240,-245)' -f 100  -z 0 -u 1 -o lsd2_decapodiformes_rootedu_final_240_249_f100_tree4
